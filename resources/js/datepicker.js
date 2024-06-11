6f825ad1dfebb52e9c151cb91dbe1c66f003e81f
Christian Vondrak <christian20599@gmail.com>
2024-06-11 16:55:08 -0400

    The datepicker shows the selected date after reloading the page and shows both dates

--- resources/js/datepicker.js
+++ resources/js/datepicker.js
@@ -1,26 +1,33 @@
-var start = moment().startOf("month");
-var end = moment().endOf("month");
+// Inicializar la variable
+var start;
+var end;
 
-function cb(start, end) {
-    $("#kt_daterangepicker_4").html(start.format("MMMM D, YYYY") + " - " + end.format("MMMM D, YYYY"));
+// Comprobar si la variable ya existe en localStorage
+if (localStorage.getItem('start')) {
+    start = localStorage.getItem('start');
+    end = localStorage.getItem('end');
+} else {
+    // Si la variable no existe, asignar inicio de mes y final de mes
+    start = moment().startOf("month");
+    end = moment().endOf("month");
 }
 
 $("#kt_daterangepicker_4").daterangepicker({
     startDate: start,
     endDate: end,
     ranges: {
-        "Today": [moment(), moment()],
-        "Yesterday": [moment().subtract(1, "days"), moment().subtract(1, "days")],
-        "Last 7 Days": [moment().subtract(6, "days"), moment()],
-        "Last 30 Days": [moment().subtract(29, "days"), moment()],
-        "This Month": [moment().startOf("month"), moment().endOf("month")],
-        "Last Month": [moment().subtract(1, "month").startOf("month"), moment().subtract(1, "month").endOf("month")]
-    }
-}, cb);
-
-$('input[name="start"]').on('apply.daterangepicker', function (ev, picker) {
-    $(this).val(picker.startDate.format('YYYY/MM/DD'));
-    $('input[name="end"]').val(picker.endDate.format('YYYY/MM/DD'));
+        'Today': [moment(), moment()],
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    },
+}, function(start, end) {
+    // Asignar valor al input start y end
+    $("#start").val(start.format('YYYY/MM/DD'));
+    $("#end").val(end.format('YYYY/MM/DD'));
+    // Guardar en localStorage el valor de las variables
+    localStorage.setItem('start', start.format('MM/DD/YYYY'));
+    localStorage.setItem('end', end.format('MM/DD/YYYY'));
 });
-
-cb(start, end);
