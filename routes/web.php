45352c7a7485b0626beed5d1c0f29f8483c6d750
Christian Vondrak <christian20599@gmail.com>
2025-04-25 19:59:51 -0400

    userDetails has been implemented into user show view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserDetailController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProjectController;
@@ -14,4 +15,11 @@ Route::middleware([
     Route::get('/home', [HomeController::class, 'index'])->name('home.index');
     Route::get('/project/{id}', [ProjectController::class, 'show'])->name('project.show');
     Route::get('/user/{id}',[UserController::class, 'show'])->name('user.show');
+    Route::get('/users',[UserController::class, 'index'])->name('user.index');
+
+    // Routes to create/update additional details
+    Route::post('/user/{user}/details',  [UserDetailController::class, 'store'])
+        ->name('user.details.store');
+    Route::put('/user/{user}/details',   [UserDetailController::class, 'update'])
+        ->name('user.details.update');
 });
